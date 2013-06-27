bd360f0edd8461c066cefdf159fc62aab5796b0e
Raimu <Raimu@192.168.1.99>
2013-06-28 03:39:37 +0900

    tes

--- Jframe/src/frameLayout/Frame.java
+++ Jframe/src/frameLayout/Frame.java
@@ -1,48 +1,46 @@
 package frameLayout;
 
-import javax.swing.JFrame;
-import net.miginfocom.swing.MigLayout;
-
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JTable;
-import javax.swing.JLabel;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import net.miginfocom.swing.MigLayout;
 
 class Frame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
 	private static String[][] data = new String[10][2];
-	static String[] column = {"名前","メッセージ"};
-	static DefaultTableModel model = new DefaultTableModel(data,column);
-	static JTable table = new JTable(model);//JTableのインスタンス作成
+	private static String[] column = {"名前","メッセージ"};
+	private static DefaultTableModel model = new DefaultTableModel(data,column);
+	private static JTable table = new JTable(model);
+	//JTableのパーツ構成
 	
-	static JPanel p = new JPanel(new MigLayout(//JPanelインスタンス作成
+	private static JPanel p = new JPanel(new MigLayout(
 			"wrap 2",
 			"[grow][grow][grow][grow][grow]"));
+	//JPanel作成
 	
+	private static JScrollPane scrollp = new JScrollPane(table);
+	//JTableをスクロールバーにセット
 	
-	public Frame(){//パネルコンポーネント配置
+	public Frame(){
 		p.add(new JLabel("名前:"),"RIGHT");
 		p.add(new JTextField(""),"span,grow");
 		p.add(new JLabel("メッセージ:"),"RIGHT");
 		p.add(new JTextField(""),"span,grow");
 		p.add(new JButton("送信"),"span,grow");
-		p.add(table,"span,grow");
+		p.add(scrollp,"span,grow");
+		//JPanelにコンポーネント配置
 	}
-	public static Frame mainFrame() {//フレーム作成　完成したフレームを戻り値に
+	public static void main(String[] args) {
 		Frame f = new Frame();
 		f.setTitle("MigLayoutTest");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(360,240);
 		f.setLocationRelativeTo(null);
-		return f;
-	}
-	public static void main(String[] args) {
-		Frame f = mainFrame();
-		f.add(p);//初期化したパネルをフレームに配置
+		//JFrameの設定
+
+		f.add(p);
+		//JPanelをJFrameにのっける
 		f.setVisible(true);
 
 	}
